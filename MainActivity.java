f91f30edbc213df1f16c8d578801b1d1635a355e
merin-nayana <53849093+merin-nayana@users.noreply.github.com>
2019-10-22 22:00:34 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,94 +1,124 @@
-package com.aboullaite.client;
+package com.example.audio;
 
-import java.util.ArrayList;
-
-import com.example.client.R;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import android.app.Activity;
-import android.os.AsyncTask;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
 
 public class MainActivity extends Activity {
+    private Button startButton,stopButton;
 
-	 private ListView mList;
-	    private ArrayList<String> arrayList;
-	    private MyCustomAdapter mAdapter;
-	    private Client mClient;
-	 
-	    @Override
-	    public void onCreate(Bundle savedInstanceState)
-	    {
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.activity_main);
-	 
-	        arrayList = new ArrayList<String>();
-	 
-	        final EditText editText = (EditText) findViewById(R.id.editText);
-	        Button send = (Button)findViewById(R.id.send_button);
-	 
-	        //relate the listView from java to the one created in xml
-	        mList = (ListView)findViewById(R.id.list);
-	        mAdapter = new MyCustomAdapter(this, arrayList);
-	        mList.setAdapter(mAdapter);
-	 
-	        // connect to the server
-	        new connectTask().execute("");
-	 
-	        send.setOnClickListener(new View.OnClickListener() {
-	            @Override
-	            public void onClick(View view) {
-	 
-	                String message = editText.getText().toString();
-	 
-	                //add the text in the arrayList
-	                //arrayList.add("c: " + message);
-	 
-	                //sends the message to the server
-	                if (mClient != null) { 
-	                    mClient.sendMessage(message);
-	                }
-	 
-	                //refresh the list
-	                mAdapter.notifyDataSetChanged();
-	                editText.setText("");
-	            }
-	        });
-	 
-	    }
-	 
-	    public class connectTask extends AsyncTask<String,String,Client> {
-	 
-	        @Override
-	        protected Client doInBackground(String... message) {
-	 
-	            //we create a Client object and
-	            mClient = new Client(new Client.OnMessageReceived() {
-	                @Override
-	                //here the messageReceived method is implemented
-	                public void messageReceived(String message) {
-	                    //this method calls the onProgressUpdate
-	                    publishProgress(message);
-	                }
-	            });
-	            mClient.run();
-	 
-	            return null;
-	        }
-	 
-	        @Override
-	        protected void onProgressUpdate(String... values) {
-	            super.onProgressUpdate(values);
-	 
-	            //in the arrayList we add the messaged received from server
-	            arrayList.add(values[0]);
-	            // notify the adapter that the data set has changed. This means that new message received
-	            // from server was added to the list
-	            mAdapter.notifyDataSetChanged();
-	        }
-	    }
+    public byte[] buffer;
+    public static DatagramSocket socket;
+    private int port=12345;
 
+    AudioRecord recorder;
+
+    private int sampleRate = 16000 ; // 44100 for music
+    private int channelConfig = AudioFormat.CHANNEL_CONFIGURATION_MONO;
+    private int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+    int minBufSize =  AudioRecord.getMinBufferSize(sampleRate, channelConfig, audioFormat);
+    private boolean status = true;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startButton = (Button) findViewById (R.id.start_button);
+        stopButton = (Button) findViewById (R.id.stop_button);
+
+        startButton.setOnClickListener (startListener);
+        stopButton.setOnClickListener (stopListener);
+
+    }
+
+    private final OnClickListener stopListener = new OnClickListener() {
+
+        @Override
+        public void onClick(View arg0) {
+            status = false;
+            recorder.release();
+            Log.d("VS","Recorder released");
+        }
+
+    };
+
+    private final OnClickListener startListener = new OnClickListener() {
+
+        @Override
+        public void onClick(View arg0) {
+            status = true;
+            startStreaming();
+        }
+
+    };
+
+    public void startStreaming() {
+
+
+        Thread streamThread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+
+                    DatagramSocket socket = new DatagramSocket();
+                    Log.d("VSa", "Socket Created");
+
+                    byte[] buffer = new byte[minBufSize];
+
+                    Log.d("VSb","Buffer created of size " + minBufSize);
+                    DatagramPacket packet;
+
+                    final InetAddress destination = InetAddress.getByName("192.168.43.54");
+                    Log.d("VSc", "Address retrieved");
+
+
+                    recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,sampleRate,channelConfig,audioFormat,minBufSize*10);
+                    Log.d("VSd", "Recorder initialized");
+
+                    recorder.startRecording();
+
+
+                    while(status == true) {
+
+
+                        //reading data from MIC into buffer
+                        minBufSize = recorder.read(buffer, 0, buffer.length);
+
+                        //putting buffer in the packet
+                        packet = new DatagramPacket (buffer,buffer.length,destination,port);
+
+                        socket.send(packet);
+                        System.out.println("MinBufferSize: " +minBufSize);
+
+
+                    }
+
+
+
+                } catch(UnknownHostException e) {
+                    Log.e("VSe", "UnknownHostException");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e("VSf", e.toString());
+                }
+            }
+
+        });
+        streamThread.start();
+    }
 }
